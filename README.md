f79096698ea9e9344702e3cc104e7c408aee13b0
Mickael Daniel <daniel.mickael@gmail.com>
2021-07-18 15:55:39 +0200

    first commit

---
+++ README.md
@@ -0,0 +1,40 @@
+## Gatsby Starter: Rocket Docs
+
+Plugin: [@rocketseat/gatsby-theme-docs fork](https://github.com/mklabs/gatsby-themes/tree/main/%40rocketseat/gatsby-theme-docs)
+
+## Docs todo
+
+2.x
+
+- [x] Rework Getting Started Section: Getting Started > Install, Configuration, Quick Start, LaunchPad Window
+- [x] Add LaunchPad page
+- [x] Update getting started page with core component
+- [x] Update AttributeSet events
+- [x] Add Projectile ability documentation
+- [ ] Redo "Working with UI" page
+  - [x] Small update
+- [ ] FAQ / Migration guide
+- [x] Update Cheat Manager page
+- [x] Update Configuration page
+- [ ] Add Gameplay Effect and Abilities definitions page
+  - [x] Started
+
+1.x
+
+- [x] Ability Queue System
+- [x] Ignore Ability Cost
+- [x] Configuration
+- [x] Cheat Manager
+- [x] Working with AI
+- [x] Working with UI
+- [ ] Logging
+- [ ] Ability Overview
+  - [ ] Gameplay Effect Container
+  - [ ] OnAbilityEnded Delegate
+  - [ ] Tasks
+- [ ] Character Overview
+  - [ ] Startup Attributes, Effects and Abilities
+  - [ ] Attribute Getters
+  - [ ] Events
+  - [ ] ASC helpers (ability activation, gameplay tags check)
+
---
+++ gatsby-config.js
@@ -0,0 +1,107 @@
+const path = require(`path`)
+
+module.exports = {
+  // pathPrefix: `/`,
+
+  siteMetadata: {
+    siteTitle: `GAS Companion Docs`,
+    defaultTitle: `GAS Companion`,
+    siteTitleShort: `GAS Companion`,
+    siteDescription: `Documentation for GAS Companion: a Gameplay Ability System Starter and Template`,
+    siteUrl: `https://gascompanion.github.io`,
+    siteAuthor: `https://mklabs.github.io`,
+    siteImage: `/banner.png`,
+    siteLanguage: `en`,
+    themeColor: `#8257E6`,
+    basePath: `/`,
+  },
+  flags: { PRESERVE_WEBPACK_CACHE: true },
+  plugins: [
+    {
+      resolve: `@mklabs/gatsby-theme-docs`,
+      options: {
+        configPath: `src/config`,
+        docsPath: `src/docs`,
+        repositoryUrl: `https://github.com/GASCompanion/GASCompanion-Documentation`,
+        branch: 'dev',
+        baseDir: ``,
+        withMdx: false,
+      },
+    },
+    {
+      resolve: `gatsby-plugin-manifest`,
+      options: {
+        name: `GAS Companion Documentation`,
+        short_name: `GAS Companion Docs`,
+        start_url: `/`,
+        background_color: `#ffffff`,
+        display: `standalone`,
+        icon: `static/favicon.png`,
+      },
+    },
+    
+    {
+      resolve: `gatsby-source-filesystem`,
+      options: {
+        name: `api`,
+        path: path.join(__dirname, `src/GASCompanionAPI`),
+      },
+    },
+
+    {
+      resolve: `gatsby-plugin-mdx`,
+      options: {
+        extensions: [`.mdx`, `.md`],
+        gatsbyRemarkPlugins: [
+          `gatsby-remark-autolink-headers`,
+          `gatsby-remark-embedder`,
+          {
+            resolve: `gatsby-remark-images`,
+            options: {
+              maxWidth: 960,
+              withWebp: true,
+              wrapperStyle: 'max-width: 960px; margin-left: initial',
+              linkImagesToOriginal: true,
+            },
+          },
+          `gatsby-remark-responsive-iframe`,
+          `gatsby-remark-copy-linked-files`,
+        ],
+        plugins: [`gatsby-remark-autolink-headers`, `gatsby-remark-images`],
+      }
+    },
+
+    `gatsby-plugin-sitemap`,
+    
+    `gatsby-transformer-sharp`, 
+    `gatsby-plugin-sharp`,
+
+    {
+      resolve: `gatsby-plugin-google-gtag`,
+      options: {
+        trackingIds: [
+          process.env.GOOGLE_ANALYTICS_ID,
+        ],
+        gtagConfig: {
+          optimize_id: "OPT_CONTAINER_ID",
+          anonymize_ip: true,
+          cookie_expires: 0,
+        },
+        pluginConfig: {
+          // Puts tracking script in the head instead of the body
+          head: false,
+          respectDNT: true,
+        },
+      },
+    },
+
+    `gatsby-plugin-remove-trailing-slashes`,
+    {
+      resolve: `gatsby-plugin-canonical-urls`,
+      options: {
+        siteUrl: `https://gascompanion.github.io`,
+      },
+    },
+    `gatsby-plugin-offline`,
+  ],
+};
